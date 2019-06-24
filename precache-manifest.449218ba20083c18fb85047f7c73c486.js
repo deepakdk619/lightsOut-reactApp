83b91a975af8b3084a3797667b55db3becb36538
Deepak Kumar <deepakdk619@gmail.com>
2019-06-24 21:52:49 +0530

    Updates

---
+++ precache-manifest.449218ba20083c18fb85047f7c73c486.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664ff17e4530bcd256ee0c19ada418a5",
+    "url": "/lightsout-reactapp/index.html"
+  },
+  {
+    "revision": "4bcc41c82a3dbbab5810",
+    "url": "/lightsout-reactapp/static/css/main.7981602e.chunk.css"
+  },
+  {
+    "revision": "dbd9b9d1d09f56a8fe13",
+    "url": "/lightsout-reactapp/static/js/2.63a3d80a.chunk.js"
+  },
+  {
+    "revision": "4bcc41c82a3dbbab5810",
+    "url": "/lightsout-reactapp/static/js/main.1d9a81c0.chunk.js"
+  },
+  {
+    "revision": "1bc36d414f515143744f",
+    "url": "/lightsout-reactapp/static/js/runtime~main.d2c3eb2d.js"
+  }
+]);
